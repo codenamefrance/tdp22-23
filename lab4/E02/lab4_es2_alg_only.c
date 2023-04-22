6c55c93497a17425a48bbdde9c660c597189332a
Francesco Pio Corrado <119980340+codenamefrance@users.noreply.github.com>
2023-04-22 14:08:24 +0200

    Update lab4_es2_alg_only.c

--- lab4/E02/lab4_es2_alg_only.c
+++ lab4/E02/lab4_es2_alg_only.c
@@ -5,37 +5,52 @@
 int abs(int d);
 void ruota(int v[maxN], int N, int P, int dir);
 
-
-int main() {
-    int v[]={1,2,3,4,5,6};
+int main(){
+    int v[maxN];
     int N=6;
-    int x;
-    int new[N];
-    int P=3;
+    int P;
+    int dir;
 
-    // turning right
-
-    for(int i=0; i<N; i++){
-        x = (i + P)%N;
-        new[x]=v[i];
+    for(int y=0; y<N; y++){
+        scanf("%d", &v[y]);
     }
-    for(int i=0; i<N; i++){
-        printf("%d ", new[i]);
-    }
-
-    printf("\n");
-    // turning left
-
-    for(int i=0; i<N; i++){
-        // equivaeìle ad uno spostamento a deestra du N-P
-        x = (i+abs(N-P))%N;
-        new[x]=v[i];
-    }
-    for(int i=0; i<N; i++){
-        printf("%d ", new[i]);
-    }
+    printf("\nOra inserisci un P<N e un valore per la direzione (>0 sx, <0 dx). Digita 0 per uscire\n");
+    scanf("%d %d", &P, &dir);
+    if(dir==0 || P==0) return 0;
+    ruota(v, N, P, dir);
 }
 
+void ruota(int v[maxN], int N, int P, int dir){
+    int x;
+    int new[N];
+
+
+    if(dir>0) {
+        // turning right
+
+        for (int i = 0; i < N; i++) {
+            x = (i + P) % N;
+            new[x] = v[i];
+        }
+        for (int i = 0; i < N; i++) {
+            printf("%d ", new[i]);
+        }
+    }
+    else{
+        // turning left
+
+
+        for (int i = 0; i < N; i++) {
+            // equivaeìle ad uno spostamento a deestra du N-P
+            x = (i + abs(N - P)) % N;
+            new[x] = v[i];
+        }
+        for (int i = 0; i < N; i++) {
+            printf("%d ", new[i]);
+        }
+
+    }
+}
 
 int abs(int d){
     if(d>0) return d;
